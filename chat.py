afa2f02d0e6380fefce73884a75350c7486f48f4
Fumikazu Fujiwara | Freddie <github@ze.gs>
2023-02-23 13:35:28 +0000

    pass pylint

--- chat.py
+++ chat.py
@@ -1,5 +1,8 @@
-import openai
+"""
+This code is an example of how to use the OpenAI Python module.
+"""
 import os
+import openai
 openai.api_key = os.environ.get('OPENAI_KEY')
 engines = openai.Engine.list()
 print(engines)
